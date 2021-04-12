9de32aae113b9f0548025966e1495651a096c3af
Drew Labrie <LabrieD2@wit.edu>
2021-04-12 01:35:14 -0400

    More checkbox validation
    First server should be working

--- ssh_process.php
+++ ssh_process.php
@@ -10,13 +10,23 @@ $username='WIT';
 $password='comp4650';
 $ssh = new Net_SSH2('192.168.2.103');
 $hostnameCheck = $_POST['firstHostname'];
+$ipCheck = $_POST['firstIP'];
+$infoCheck = $_POST['firstInfo'];
 
 if (!$ssh->login($username, $password)){
 	exit('Login Failed');
 }
 
-if (!$hostnameCheck){
+if ($hostnameCheck){
 	echo $ssh->exec('hostname');
+}
+
+if ($ipCheck){
+	echo $ssh->exec('ifconfig -a');
+}
+
+if ($infoCheck){
+	echo $ssh->exec('lscpu');
 }
 
 echo $ssh->exec($command);
