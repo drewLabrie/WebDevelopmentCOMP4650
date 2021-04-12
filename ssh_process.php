b9c2dc027828a893ebd60016dd5348b705d1cc2f
Drew Labrie <LabrieD2@wit.edu>
2021-04-12 02:01:45 -0400

    php null value validation

--- ssh_process.php
+++ ssh_process.php
@@ -4,32 +4,38 @@ include('Net/SSH2.php'); //must include fpr phpseclib
 //$username=$_POST['username'];
 //$password=$_POST['pass'];
 //$ip=$_POST['ipAddress'];
-$command=$_POST['command']; //test demo purposes
 
 $username='WIT';
 $password='comp4650';
 $ssh = new Net_SSH2('192.168.2.103');
-$hostnameCheck = $_POST['firstHostname'];
-$ipCheck = $_POST['firstIP'];
-$infoCheck = $_POST['firstInfo'];
-
 if (!$ssh->login($username, $password)){
 	exit('Login Failed');
 }
-echo "";
-if ($hostnameCheck){
-	echo $ssh->exec('hostname');
+if (isset($_POST['firstHostname']) && !empty($_POST['firstHostname'])){
+	$hostnameCheck = $_POST['firstHostname'];
+	if ($hostnameCheck){
+		echo $ssh->exec('hostname');
+	}
 }
-echo "";
-if ($ipCheck){
-	echo $ssh->exec('ifconfig -a');
-}
-echo "";
-if ($infoCheck){
-	echo $ssh->exec('lscpu');
-}
-echo "";
-echo $ssh->exec($command);
 
+if (isset($_POST['firstIP']) && !empty($_POST['firstIP'])){
+	$ipCheck = $_POST['firstIP'];
+	if ($ipCheck){
+		echo $ssh->exec('ifconfig -a');
+	}
+}
+
+if (isset($_POST['firstInfo']) && !empty($_POST['firstInfo'])){
+	$infoCheck = $_POST['firstInfo'];
+	if ($infoCheck){
+		echo $ssh->exec('lscpu');
+	}
+}
+
+if (isset($_POST['command']) && !empty($_POST['command'])){
+	echo $ssh->exec($command);
+	$command=$_POST['command'];
+	
+}
 
 ?>
