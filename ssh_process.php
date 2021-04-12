aa91d74059ea3f9d887b5af441b1ebed8bcfc997
Drew Labrie <LabrieD2@wit.edu>
2021-04-12 01:27:56 -0400

    Hostname checkbox test
    Uploading test code for checkbox validation and printing

--- ssh_process.php
+++ ssh_process.php
@@ -1,16 +1,23 @@
 <?php
 include('Net/SSH2.php'); //must include fpr phpseclib
 
-$username=$_POST['username'];
-$password=$_POST['pass'];
-$ip=$_POST['ipAddress'];
+//$username=$_POST['username'];
+//$password=$_POST['pass'];
+//$ip=$_POST['ipAddress'];
 $command=$_POST['command']; //test demo purposes
 
-$ssh = new Net_SSH2($ip);
+$username='WIT';
+$password='comp4650';
+$ssh = new Net_SSH2('192.168.2.103');
+$hostnameCheck = $_POST['firstHostname'];
 
 if (!$ssh->login($username, $password)){
 	exit('Login Failed');
-} 
+}
+
+if (!$hostnameCheck){
+	echo $ssh->exec('hostname');
+}
 
 echo $ssh->exec($command);
 
