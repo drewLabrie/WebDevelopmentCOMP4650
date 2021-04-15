d0c53109121570107061a69b8a1c01d8e5330c63
wongm5 <44271563+wongm5@users.noreply.github.com>
2021-04-15 00:56:07 -0400

    added jquery ajax script
    added parent divs for styling purposes

--- mainForm.php
+++ mainForm.php
@@ -4,6 +4,7 @@
 		<title>Network Manager</title>
 		<link rel="stylesheet" href="webDevForm2.css" />
 		<meta name="viewport" content="width=device-width,initial-scale=1">
+		<script src="https://code.jquery.com/jquery-3.5.0.js"></script>
 	</head>
 	<!--do php login validation to connect to database-->
 	
@@ -58,17 +59,34 @@ if (!$validated){ ?>
 <?php } else{ ?>
 	
 	<body>
-		<form method="post" action="ssh_process.php" class="form-container">
+		<form id ="myForm" form method="post" action="ssh_process.php" class="form-container">
 			<div class = "grid-container">
 			<div class="card" style="width:100%;max-width:500px;">
 				<div class="container">
 				<h1>FTP Server</h1>
 				<h3>IP Address: 192.168.2.103</h3>
-				<h4>Request Hostname?<input type="checkbox" class="requestButton" name="firstHostname"></h4>
-				<h4>Request Addressing Info?<input type="checkbox" class="requestButton" name="firstIP"></h4>
-				<h4>Request System Usage?<input type="checkbox" class="requestButton" name="firstInfo"></h4>
-				<h4>Request FTP Status?<input type="checkbox" class="requestButton" name="firstService"></h4>
-				<h4>Custom Command?<input type="text" class="requestButton" name="firstCommand"></h4>
+				<div class="myDiv">
+					<h4>Request Hostname?<input type="checkbox" class="requestButton" name="firstHostname"></h4>
+					<h5 id = "hostname1"></h5>
+				</div>
+				<div class="myDiv">
+					<h4>Request Addressing Info?<input type="checkbox" class="requestButton" name="firstIP"></h4>
+					<h5 id = "ip1"></h5>
+					<h5 id = "mac1"></h5>
+				</div>
+				<div class="myDiv">
+					<h4>Request System Usage?<input type="checkbox" class="requestButton" name="firstInfo"></h4>
+					<h5 id = "cpu1"></h5>
+					<h5 id = "mem1"></h5>
+				</div>
+				<div class="myDiv">
+					<h4>Request FTP Status?<input type="checkbox" class="requestButton" name="firstService"></h4>
+					<h5 id = "service1"></h5>
+				</div>
+				<div class = "myDiv">
+					<h4>Custom Command?<input type="text" class="requestButton" name="firstCommand"></h4>
+					<h5 id = "command1"></h5>
+				</div>
 				<button type="submit" class="btn">Request Information</button>
 				</div>
 			</div>
@@ -77,24 +95,58 @@ if (!$validated){ ?>
 				<div class="container">
 				<h1>Mail Server</h1>
 				<h3>IP Address: 192.168.2.30</h3>
-				<h4>Request Hostname?<input type="checkbox" class="requestButton" name="secondHostname"></h4>
-				<h4>Request Addressing Info?<input type="checkbox" class="requestButton" name="secondIP"></h4>
-				<h4>Request System Usage?<input type="checkbox" class="requestButton" name="secondInfo"></h4>
-				<h4>Request SMTP Status?<input type="checkbox" class="requestButton" name="secondService"></h4>
-				<h4>Custom Command?<input type="text" class="requestButton" name="secondCommand"></h4>
+				<div class="myDiv">
+					<h4>Request Hostname?<input type="checkbox" class="requestButton" name="secondHostname"></h4>
+					<h5 id = "hostname2"></h5>
+				</div>
+				<div class="myDiv">
+					<h4>Request Addressing Info?<input type="checkbox" class="requestButton" name="secondIP"></h4>
+					<h5 id = "ip2"></h5>
+					<h5 id = "mac2"></h5>
+				</div>
+				<div class="myDiv">
+					<h4>Request System Usage?<input type="checkbox" class="requestButton" name="secondInfo"></h4>
+					<h5 id = "cpu2"></h5>
+					<h5 id = "mem2"></h5>
+				</div>
+				<div class="myDiv">
+					<h4>Request FTP Status?<input type="checkbox" class="requestButton" name="secondService"></h4>
+					<h5 id = "service2"></h5>
+				</div>
+				<div class = "myDiv">
+					<h4>Custom Command?<input type="text" class="requestButton" name="secondCommand"></h4>
+					<h5 id = "command2"></h5>
+				</div>
 				<button type="submit" class="btn">Request Information</button>
 				</div>
 			</div>
 
 			<div class="card" style="width:100%;max-width:500px;">
-				<div class="container">
+				<div class="container" >
 				<h1>Random Server</h1>
 				<h3>IP Address: blah blah blah</h3>
-				<h4>Request Hostname?<input type="checkbox" class="requestButton"></h4>
-				<h4>Request IP Address?<input type="checkbox" class="requestButton"></h4>
-				<h4>Request System Usage?<input type="checkbox" class="requestButton"></h4>
-				<h4>Custom Command?<input type="text" class="requestButton" name="command"></h4>
-
+				<div class="myDiv">
+					<h4>Request Hostname?<input type="checkbox" class="requestButton" name="thirdHostname"></h4>
+					<h5 id = "hostname3"></h5>
+				</div>
+				<div class="myDiv">
+					<h4>Request Addressing Info?<input type="checkbox" class="requestButton" name="thirdIP"></h4>
+					<h5 id = "ip3"></h5>
+					<h5 id = "mac3"></h5>
+				</div>
+				<div class="myDiv">
+					<h4>Request System Usage?<input type="checkbox" class="requestButton" name="thirdInfo"></h4>
+					<h5 id = "cpu3"></h5>
+					<h5 id = "mem3"></h5>
+				</div>
+				<div class="myDiv">
+					<h4>Request FTP Status?<input type="checkbox" class="requestButton" name="thirdService"></h4>
+					<h5 id = "service3"></h5>
+				</div>
+				<div class = "myDiv">
+					<h4>Custom Command?<input type="text" class="requestButton" name="thirdCommand"></h4>
+					<h5 id = "command3"></h5>
+				</div>
 				<button type="submit" class="btn">Request Information</button>	
 				</div>
 			</div>
@@ -103,21 +155,71 @@ if (!$validated){ ?>
 				<div class="container">
 				<h1>Random Server</h1>
 				<h3>IP Address: blah blah blah</h3>
-				<h4>Request hostname?<input type="checkbox" class="requestButton"></h4>
-				<h4>Request ip information?<input type="checkbox" class="requestButton"></h4>
-				<h4>Request server info<input type="checkbox" class="requestButton"></h4>
-				<h4>Custom Command?<input type="text" class="requestButton" name="command"></h4>
+				<div class="myDiv">
+					<h4>Request Hostname?<input type="checkbox" class="requestButton" name="fourthHostname"></h4>
+					<h5 id = "hostname4"></h5>
+				</div>
+				<div class="myDiv">
+					<h4>Request Addressing Info?<input type="checkbox" class="requestButton" name="fourthIP"></h4>
+					<h5 id = "ip4"></h5>
+					<h5 id = "mac4"></h5>
+				</div>
+				<div class="myDiv">
+					<h4>Request System Usage?<input type="checkbox" class="requestButton" name="fourthInfo"></h4>
+					<h5 id = "cpu4"></h5>
+					<h5 id = "mem4"></h5>
+				</div>
+				<div class="myDiv">
+					<h4>Request FTP Status?<input type="checkbox" class="requestButton" name="fourthService"></h4>
+					<h5 id = "service4"></h5>
+				</div>
+				<div class = "myDiv">
+					<h4>Custom Command?<input type="text" class="requestButton" name="fourthCommand"></h4>
+					<h5 id = "command4"></h5>
+				</div>
 				<button type="submit" class="btn">Request Information</button>
 				</div>
 			</div>
+
 			</div>
-
 				<button type="submit" class="btn">Request Information</button>
-
+			</fieldset>
 		</form>
 
 	</body>
-	
+		<script>
+
+      $(document).ready(function(){
+     	 $("#myForm").submit(function(e){
+      	 	alert("form is submitted")
+      		 e.preventDefault();
+      		 $.ajax({
+      		 	url: $(this).attr('action'),
+      		 	type: 'post',
+      		 	data: $(this).serialize(),
+      		 	dataType: 'json',
+      		 	success: function(data){
+      		 		$("#hostname1").html(data[0]);
+      		 		$("#ip1").html(data[1]);
+      		 		$("#mac1").html(data[2]);
+      		 		$("#cpu1").html(data[3]);
+      		 		$("#mem1").html(data[4]);
+      		 		$("#service1").html(data[5]);
+      		 		$("#command1").html(data[6]);
+      		 		$("#hostname2").html(data[7]);
+      		 		$("#ip2").html(data[8]);
+      		 		$("#mac2").html(data[9]);
+      		 		$("#cpu2").html(data[10]);
+      		 		$("#mem2").html(data[11]);
+      		 		$("#service2").html(data[12]);
+      		 		$("#command2").html(data[13]);
+      		 	}
+      		 });
+
+     	 });
+      })
+
+		</script>
 <?php } ?>
 	
 	
