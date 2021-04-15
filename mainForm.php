8b1358b8fa82b4986fe93aac6f8c211e4bcc4f23
Drew Labrie <LabrieD2@wit.edu>
2021-04-14 22:05:26 -0400

    Testing database connectivity

--- mainForm.php
+++ mainForm.php
@@ -9,7 +9,7 @@
 	
 	
 <?php 
-	$validated = true;
+	$validated = false;
 
 if (!$validated){ ?>
 	<body>
@@ -37,7 +37,26 @@ if (!$validated){ ?>
 		<div class="bottomHeaderMembers">Group Members: Drew Labrie, Marcus Wong, Prarthna Bhaththiwala</div>
 	</body>
 	
-<?php } else{ ?>
+<?php
+
+	$message="";
+	if(count($_POST)>0){
+		$conn = mysqli_connect("192.168.2.10", "WIT", "comp4650", "WebDevelopmentDB");
+		$result = mysqli_query($conn, "SELECT * FROM UserAccounts WHERE user_name='" . $_POST["userName"] . "' and password = '". $_POST["pass"]."'");
+		$count  = mysqli_num_rows($result);
+		if($count==0) {
+		$message = "Invalid Username or Password!";
+	} else {
+		$message = "You are successfully authenticated!";
+	}
+	
+	console.log($message);
+}
+
+
+
+
+ } else{ ?>
 	
 	<body>
 		<form method="post" action="ssh_process.php" class="form-container">
